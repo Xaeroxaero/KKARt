1265c6b6d78f885b9ec78787bc81a9090705902f
xaeroxaero <xaeroxaero@gmail.com>
2017-03-26 20:29:05 +0200

    start

--- products/models.py
+++ products/models.py
@@ -5,10 +5,10 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 from pygments import highlight
 
+
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
-
 
 class Product(models.Model):
     owner = models.ForeignKey('auth.User', related_name='products')
@@ -16,6 +16,7 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     image_source = models.TextField(default='')
+    metod = models.CharField(choices=LANGUAGE_CHOICES, default='', max_length=100)
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
@@ -30,6 +31,7 @@ class Product(models.Model):
         lexer = get_lexer_by_name(self.language)
         linenos = self.linenos and 'table' or False
         options = self.title and {'title': self.title} or {}
+
         formatter = HtmlFormatter(style=self.style, linenos=linenos,
                                   full=True, **options)
         self.highlighted = highlight(self.code, lexer, formatter)
--- products/serializers.py
+++ products/serializers.py
@@ -9,7 +9,7 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = ('url', 'id', 'highlight', 'owner',
-                  'title', 'image_source', 'linenos', 'language', 'style')
+                  'title','metod', 'image_source', 'linenos', 'language', 'style')
 
 
 
