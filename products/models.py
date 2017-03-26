1b263c1999b646be58f5cef48e7c5bbe62af3c29
xaeroxaero <xaeroxaero@gmail.com>
2017-03-26 20:42:27 +0200

    start

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ from pygments import highlight
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+malowanie = [(1, 'akryl'), (2, 'olejna'), (3, 'akwarela')]
 
 class Product(models.Model):
     owner = models.ForeignKey('auth.User', related_name='products')
@@ -16,7 +17,7 @@ class Product(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     image_source = models.TextField(default='')
-    metod = models.CharField(choices=LANGUAGE_CHOICES, default='', max_length=100)
+    metod = models.CharField(choices=malowanie, default='', max_length=100)
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
